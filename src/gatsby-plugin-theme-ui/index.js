4d668598e94be10caf8e2c146a79545887b77803
Charles Hood <charles.hood@gmail.com>
2020-12-21 23:31:58 -0500

    Update index.js

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -1,22 +1,22 @@
 export default {
   colors: {
-    text: '#000',
-    background: '#fff',
-    primary: '#5C2941',
-    accent: '#fff',
-    muted: 'rgba(0, 0, 0, 0.7)',
-    cardBg: '#fff',
-    borderColor:"#540229",
+    text: '#f5f5f5',
+    background: '#111',
+    primary: '#252525',
+    accent: '#5C2941',
+    muted: 'rgba(255, 255, 255, 0.7)',
+    buttonAccent: '#fff',
+    cardBg: '#252525',
+    borderColor:"#888",
     modes: {
       dark: {
-        text: '#f5f5f5',
-        background: '#111',
-        primary: '#252525',
-        accent: '#5C2941',
-        muted: 'rgba(255, 255, 255, 0.7)',
-        buttonAccent: '#fff',
-        cardBg: '#252525',
-        borderColor:"#888"
+        text: '#000',
+        background: '#fff',
+        primary: '#5C2941',
+        accent: '#fff',
+        muted: 'rgba(0, 0, 0, 0.7)',
+        cardBg: '#fff',
+        borderColor:"#540229"
       },
     }
   },
